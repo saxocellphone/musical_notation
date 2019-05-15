c52f7cbf69ffdd605c3df558615bdf856612cb80
saxocellphone <nazzav923@gmail.com>
2019-05-15 19:17:24 -0400

    initial commit

---
+++ chord.js
@@ -0,0 +1,5 @@
+class Chord {
+
+}
+
+export default Chord;
---
+++ index.js
@@ -0,0 +1,9 @@
+import { Chord } from './chord';
+
+export default {
+    Chord
+};
+
+export function printMsg() {
+    console.log("Using music notation package");
+}
